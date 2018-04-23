b25c63b36fa40bf54c2559f39c5cb9ab6f661d18
Big Man Tyrone <chaerulhafidz@gmail.com>
2018-04-23 10:45:47 +0700

    Update and rename signup.html to signup.php

--- SignUp/signup.php
+++ SignUp/signup.php
@@ -1,72 +1,78 @@
 <!DOCTYPE html>
 <html>
 <style>
-body {font-family: Arial, Helvetica, sans-serif;}
-* {box-sizing: border-box}
+	body {font-family: Arial, Helvetica, sans-serif;}
+	* {box-sizing: border-box}
 
 
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 15px;
-    margin: 5px 0 22px 0;
-    display: inline-block;
-    border: none;
-    background: #f1f1f1;
-}
+	input[type=text], input[type=password] {
+	    width: 100%;
+	    padding: 15px;
+	    margin: 5px 0 22px 0;
+	    display: inline-block;
+	    border: none;
+	    background: #f1f1f1;
+	}
 
-input[type=text]:focus, input[type=password]:focus {
-    background-color: #ddd;
-    outline: none;
-}
+	input[type=text]:focus, input[type=password]:focus {
+	    background-color: #ddd;
+	    outline: none;
+	}
 
-hr {
-    border: 1px solid #f1f1f1;
-    margin-bottom: 25px;
-}
+	hr {
+	    border: 1px solid #f1f1f1;
+	    margin-bottom: 25px;
+	}
 
 
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-    opacity: 0.9;
-}
+	button {
+	    background-color: #4CAF50;
+	    color: white;
+	    padding: 14px 20px;
+	    margin: 8px 0;
+	    border: none;
+	    cursor: pointer;
+	    width: 100%;
+	    opacity: 0.9;
+	}
 
-button:hover {
-    opacity:1;
-}
+	button:hover {
+	    opacity:1;
+	}
 
-/* Extra styles for the cancel button */
-.cancelbtn {
-    padding: 14px 20px;
-    background-color: #f44336;
-}
+	/* Extra styles for the cancel button */
+	.cancelbtn {
+	    padding: 14px 20px;
+	    background-color: #f44336;
+	}
 
-.cancelbtn, .signupbtn {
-  float: left;
-  width: 50%;
-}
+	.cancelbtn, .signupbtn {
+	  float: left;
+	  width: 50%;
+	}
 
-.container {
-    padding: 16px;
-}
+	.container {
+	    padding: 16px;
+	}
 
-.clearfix::after {
-    content: "";
-    clear: both;
-    display: table;
-}
+	.clearfix::after {
+	    content: "";
+	    clear: both;
+	    display: table;
+	}
 
-@media screen and (max-width: 300px) {
-    .cancelbtn, .signupbtn {
-       width: 100%;
-    }
-}
+	@media screen and (max-width: 300px) {
+	    .cancelbtn, .signupbtn {
+	       width: 100%;
+	    }
+	}
 </style>
+
+<?php
+	include "connect.php"; 
+	include "SignUpDB.php";
+?>
+	
 <body>
 
 <form action="/action_page.php" style="border:1px solid #ccc">
@@ -75,14 +81,14 @@ button:hover {
     <p>Please fill in this form to create an account.</p>
     <hr>
 	
-	<label for="name"><b>Name</b></label>
+    x<label for="name"><b>Name</b></label>
     <input type="text" placeholder="Enter Name" name="name" required>
     
     <label for="phone_number"><b>Phone Number</b></label>
     <input type="text" placeholder="Enter Phone Number" name="phone_number" required>
 	
 	<label for="address"><b>Address</b></label>
-    <input type="text" placeholder="Enter Address" name="Address" rows=4 required>
+    <input type="text" placeholder="Enter Address" name="address" rows=4 required>
     
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>
