7e6b6f1518de9f99e9f042a42c19743d687bae82
Big Man Tyrone <chaerulhafidz@gmail.com>
2018-05-16 13:26:59 +0700

    no use

--- connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$main = mysqli_connect('localhost','root',''); 						<!-- buat konek ke database -->
-mysqli_select_db($main,'nama databasenya(ganti!)');					<!-- ke database yang diinginkan.. -->
-?>
