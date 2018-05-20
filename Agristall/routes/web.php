b9df197a5a4dc3978c64bc2804e525d246accc6e
horsekom <ajifajar23@gmail.com>
2018-05-20 18:57:25 +0700

    lara error ganti native aja kuy

--- Agristall/routes/web.php
+++ Agristall/routes/web.php
@@ -11,6 +11,60 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'FrontEndHomeController@getMaster')->name('frontEnd.master');
+
+Route::get('/login', function(){
+  return view('frontEnd.login');
+});
+
+Route::get('/kitchen', function(){
+  return view('frontEnd.kitchen');
+});
+
+Route::get('/drinks', function(){
+  return view('frontEnd.drinks');
+});
+
+Route::get('/about', function(){
+  return view('frontEnd.about');
+});
+
+Route::get('/bread', function(){
+  return view('frontEnd.bread');
+});
+
+Route::get('/frozen', function(){
+  return view('frontEnd.frozen');
+});
+
+Route::get('/household', function(){
+  return view('frontEnd.household');
+});
+
+Route::get('/kitchen', function(){
+  return view('frontEnd.kitchen');
+});
+
+Route::get('/mail', function(){
+  return view('frontEnd.mail');
+});
+
+Route::get('/pet', function(){
+  return view('frontEnd.pet');
+});
+
+Route::get('/products', function(){
+  return view('frontEnd.products');
+});
+
+Route::get('/vegetables', function(){
+  return view('frontEnd.household');
+});
+
+Route::get('/single', function(){
+  return view('frontEnd.single');
 });
