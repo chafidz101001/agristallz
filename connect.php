81c04252d1887d73faa4474b7c5e4019eb06366d
ghifarihaekal <ghifarihaekal7@gmail.com>
2018-06-01 22:59:47 +0700

    sapu jagat pt. 3
    Co-Authored-By: Ryujin No Ken Wo Kurae <chaerulhafidz@gmail.com>

---
+++ connect.php
@@ -0,0 +1,195 @@
+<?php
+
+
+
+$db = mysqli_connect('localhost', 'root', '', 'agristall');
+$errors = array();
+$nama = "";
+$email = "";
+$alamat = "";
+$no_tlp = "";
+
+if (isset($_POST['register_user'])) {
+  // receive all input values from the form
+  $nama = mysqli_real_escape_string($db, $_POST['Nama']);
+  $email = mysqli_real_escape_string($db, $_POST['Email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['Password']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['Password_cnf']);
+  $alamat = mysqli_real_escape_string($db, $_POST['Alamat']);
+  $no_tlp = mysqli_real_escape_string($db, $_POST['Telefon']);
+ /* 
+  echo $nama;
+  echo $email;
+  echo $password_1;
+  echo $alamat;
+  echo $kota;
+  echo $kode_pos;
+  echo $no_tlp;
+*/
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($nama)) { array_push($errors, "Masukkan Nama!"); }
+  if (empty($email)) { array_push($errors, "Masukkan Email!"); }
+  if (empty($password_1)) { array_push($errors, "Masukkan Password!"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "Password Tidak Sama!");
+	}
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM user WHERE nama='$nama' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['nama'] === $nama) {
+      array_push($errors, "Anda sudah pernah mendaftar dengan identitas ini!");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Email sudah terpakai!");
+    }
+  }
+
+    if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO user (nama, password, email, alamat, no_tlp) 
+  			  VALUES ('$nama', '$password', '$email', '$alamat', '$no_tlp')";
+  	mysqli_query($db, $query);
+
+    
+    /*
+  	$_SESSION['nama'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');*/
+  }
+}
+
+if (isset($_POST['log_user'])) {
+  $email = mysqli_real_escape_string($db, $_POST['Email']);
+  $password = mysqli_real_escape_string($db, $_POST['Password']);
+  //echo $password;
+
+  if (empty($email)) {
+    array_push($errors, "Masukkan Email Anda!");
+  }
+  if (empty($password)) {
+    array_push($errors, "Masukkan Password Anda!");
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password);
+    $query = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) > 0) {
+      $_SESSION['email'] = $email;
+      $_SESSION['success'] = "Anda sudah masuk.";
+      header('location: index.php');
+      echo $password;
+    }else {
+      array_push($errors, "Email atau Password Anda Salah!");
+    }
+  }
+}
+
+if (isset($_POST['edit_user'])) {
+   // receive all input values from the form
+  $nama = mysqli_real_escape_string($db, $_POST['Nama']);
+  $email = mysqli_real_escape_string($db, $_POST['Email']);
+  $alamat = mysqli_real_escape_string($db, $_POST['Alamat']);
+  $no_tlp = mysqli_real_escape_string($db, $_POST['Telp']);
+
+  $sql = "SELECT * FROM user WHERE email = '" . $_SESSION['email'] . "'";
+  $result = mysqli_query($db,$sql);
+  $row = mysqli_fetch_array($result);
+  //echo $result;
+
+
+  $user_check_query = "SELECT * FROM user WHERE nama='$nama' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+      if ($user['nama'] === $nama && $user['nama'] !== $row['nama']) {
+      array_push($errors, "Nama Anda Sudah Terpakai");
+      }
+
+      if ($user['email'] === $email && $user['email'] !== $row['email']) {
+        array_push($errors, "Email sudah terpakai!");
+      }
+  }
+
+  if (count($errors) == 0) {
+
+    $query = "UPDATE user SET nama = '$nama', email = '$email', alamat = '$alamat', no_tlp = '$no_tlp' WHERE email = '" . $_SESSION['email'] . "'";
+          
+    mysqli_query($db, $query);
+
+
+  }
+
+}
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
+
+
+if (isset($_POST['forgot_user'])) {
+  $email = mysqli_real_escape_string($db, $_POST['Email']);
+
+  $query = "SELECT * FROM user WHERE email= '$email'";
+    $res = mysqli_query($db, $query);
+
+
+  $r = mysqli_fetch_assoc($res);
+  $password = generateRandomString();
+  $passwordHashed = md5($password);
+  $query = "UPDATE user SET password = '$passwordHashed' WHERE email = '$email'";
+  mysqli_query($db,$query);
+
+  $to = $r['email'];
+  $subject = "Your Recovered Password";
+   
+  $message = "Please use this password to login: " . $password;
+  $headers = "From : Agristall's Admins";
+  mail($to, $subject, $message, $headers);
+
+}
+
+if (isset($_POST['res_pass'])) {
+  $password = md5(mysqli_real_escape_string($db, $_POST['Password']));
+  $password_1 = mysqli_real_escape_string($db, $_POST['Password_new']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['Password_new_cnf']);
+
+  $pass_check_query = "SELECT password FROM user WHERE email= '" . $_SESSION['email'] . "'";
+  $result = mysqli_query($db, $pass_check_query);
+  $passcheck = mysqli_fetch_assoc($result);
+
+  if ($password != $passcheck) {
+  array_push($errors, "Password Anda Salah");
+
+  }
+  if ($password_1 != $password_2) {
+  array_push($errors, "Password Tidak Sama!");
+  }
+
+  $passwordhash = md5($password_1);
+
+
+  $query = "UPDATE user SET password = '$passwordhash' WHERE email = '" . $_SESSION['email'] . "'";
+  mysqli_query($db,$query);
+
+}
+
+
+?>
---
+++ headerNat.php
@@ -0,0 +1,84 @@
+<!-- header -->
+	<div class="agileits_header">
+		<div class="w3l_offers">
+			<a href="products.html">Penawaran Spesial</a>
+		</div>
+		<div class="w3l_search">
+			<form action="#" method="post">
+				<input type="text" name="Product" value="Cari Produk..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Cari Produk...';}" required="">
+				<input type="submit" value=" ">
+			</form>
+		</div>
+		<div class="pull-right" style="padding:2px;">
+			<ul>
+				<li class="dropdown profile_details_drop">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i><span class="caret"></span></a>
+					<div class="mega-dropdown-menu">
+						<div class="w3ls_vegetables">
+							<ul class="dropdown-menu drp-mnu">
+								<?php if (isset($_SESSION['email'])) : ?>
+								<li><?php 
+										$sql = "SELECT nama FROM user WHERE email = '" . $_SESSION['email'] . "'";
+										$result = mysqli_query($db,$sql);
+										//echo $result;
+										$row = mysqli_fetch_array($result);
+										echo $row['nama'];								?></li>
+										<li><a href="profile.php">Profil</a></li>
+								<li><a href="resPassword.php">Ganti Password</a></li>
+								<li><a href="history.php">Pembelian</a></li>
+								<li><a href="index.php?logout_user='1'">Logout</a></li>
+							<?php endif ?>
+
+							<?php if (!isset($_SESSION['email'])) : ?> 
+								<li><a href="login.php">Login</a></li>
+								<li><a href="register.php">Sign Up</a></li>
+							<?php endif ?>
+							</ul>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+<!-- script-for sticky-nav -->
+	<script>
+	$(document).ready(function() {
+		 var navoffeset=$(".agileits_header").offset().top;
+		 $(window).scroll(function(){
+			var scrollpos=$(window).scrollTop(); 
+			if(scrollpos >=navoffeset){
+				$(".agileits_header").addClass("fixed");
+			}else{
+				$(".agileits_header").removeClass("fixed");
+			}
+		 });
+		 
+	});
+	</script>
+<!-- //script-for sticky-nav -->
+	<div class="logo_products">
+		<div class="container">
+			<div class="w3ls_logo_products_left">
+				<h1><a href="index.php"><span>Halaman</span> Utama</a></h1>
+			</div>
+			<div class="w3ls_logo_products_left1">
+				<ul class="special_items">
+					<li><a href="events.html">Events</a><i>/</i></li>
+					<li><a href="about.html">About Us</a><i>/</i></li>
+					<li><a href="products.html">Best Deals</a><i>/</i></li>
+					<li><a href="services.html">Services</a></li>
+				</ul>
+			</div>
+			<div class="w3ls_logo_products_left1">
+				<ul class="phone_email">
+					<li><i class="fa fa-phone" aria-hidden="true"></i>(+0858) 61934625</li>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"></i><a href="mailto:store@grocery.com">anonymouse14@gmail.com</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //header -->
+<!-- products-breadcrumb -->
+	
