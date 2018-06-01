b9578e6a301019a15e6f698367e646a38966190e
ghifarihaekal <ghifarihaekal7@gmail.com>
2018-06-01 23:07:25 +0700

    well done

--- done.php
+++ done.php
@@ -11,7 +11,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Grocery Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+<meta name="keywords" content="Grocery Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -19,7 +19,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <!-- font-awesome icons -->
-<link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" /> 
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
 <!-- //font-awesome icons -->
 <!-- js -->
 <script src="js/jquery-1.11.1.min.js"></script>
@@ -31,7 +31,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="text/javascript" src="js/easing.js"></script>
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
-		$(".scroll").click(function(event){		
+		$(".scroll").click(function(event){
 			event.preventDefault();
 			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
 		});
@@ -39,7 +39,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </script>
 <!-- start-smoth-scrolling -->
 </head>
-	
+
 <body>
 <!-- header -->
 	<div class="agileits_header">
@@ -52,7 +52,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<input type="submit" value=" ">
 			</form>
 		</div>
-		<div class="product_list_header">  
+		<div class="product_list_header">
 			<form action="#" method="post" class="last">
                 <fieldset>
                     <input type="hidden" name="cmd" value="_cart" />
@@ -68,11 +68,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="mega-dropdown-menu">
 						<div class="w3ls_vegetables">
 							<ul class="dropdown-menu drp-mnu">
-								<li><a href="login.html">Login</a></li> 
+								<li><a href="login.html">Login</a></li>
 								<li><a href="login.html">Sign Up</a></li>
 							</ul>
-						</div>                  
-					</div>	
+						</div>
+					</div>
 				</li>
 			</ul>
 		</div>
@@ -86,14 +86,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	$(document).ready(function() {
 		 var navoffeset=$(".agileits_header").offset().top;
 		 $(window).scroll(function(){
-			var scrollpos=$(window).scrollTop(); 
+			var scrollpos=$(window).scrollTop();
 			if(scrollpos >=navoffeset){
 				$(".agileits_header").addClass("fixed");
 			}else{
 				$(".agileits_header").removeClass("fixed");
 			}
 		 });
-		 
 	});
 	</script>
 <!-- //script-for sticky-nav -->
@@ -142,22 +141,22 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				  </button>
-			   </div> 
+			   </div>
 			   <!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 					<ul class="nav navbar-nav nav_1">
 						<li><a href="products.html">Branded Foods</a></li>
 						<li><a href="household.html">Households</a></li>
 						<li class="dropdown mega-dropdown active">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Veggies & Fruits<span class="caret"></span></a>				
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Veggies & Fruits<span class="caret"></span></a>
 							<div class="dropdown-menu mega-dropdown-menu w3ls_vegetables_menu">
 								<div class="w3ls_vegetables">
-									<ul>	
+									<ul>
 										<li><a href="vegetables.html">Vegetables</a></li>
 										<li><a href="vegetables.html">Fruits</a></li>
 									</ul>
-								</div>                  
-							</div>				
+								</div>
+							</div>
 						</li>
 						<li><a href="kitchen.html">Kitchen</a></li>
 						<li><a href="short-codes.html">Short Codes</a></li>
@@ -169,8 +168,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										<li><a href="drinks.html">Soft Drinks</a></li>
 										<li><a href="drinks.html">Juices</a></li>
 									</ul>
-								</div>                  
-							</div>	
+								</div>
+							</div>
 						</li>
 						<li><a href="pet.html">Pet Food</a></li>
 						<li class="dropdown">
@@ -181,8 +180,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 										<li><a href="frozen.html">Frozen Snacks</a></li>
 										<li><a href="frozen.html">Frozen Nonveg</a></li>
 									</ul>
-								</div>                  
-							</div>	
+								</div>
+							</div>
 						</li>
 						<li><a href="bread.html">Bread & Bakery</a></li>
 					</ul>
@@ -192,25 +191,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="w3l_banner_nav_right">
 <!-- about -->
 		<div class="privacy about">
-			<h3>About Us</h3>
-			<p class="animi">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis 
-				praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias 
-				excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui 
-				officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem 
-				rerum facilis est et expedita distinctio.</p>
+			<h3>Transaksi Anda Berhasil</h3>
+			<p class="animi">Terima kasih telah berbelanja di agristall. Pesanan akan kami antar langsung ke alamat anda.
+				Pembayaran diberikan pada kurir pengantar produk kami di saat barang diantar. Selamat menikmati produk terbaik dari alam.</p>
 			<div class="agile_about_grids">
 				<div class="col-md-6 agile_about_grid_right">
 					<img src="images/31.jpg" alt=" " class="img-responsive" />
-				</div>
-				<div class="col-md-6 agile_about_grid_left">
-					<ol>
-						<li>laborum et dolorum fuga</li>
-						<li>corrupti quos dolores et quas</li>
-						<li>est et expedita distinctio</li>
-						<li>deleniti atque corrupti quos</li>
-						<li>excepturi sint occaecati cupiditate</li>
-						<li>accusamus et iusto odio</li>
-					</ol>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
@@ -221,122 +207,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	</div>
 <!-- //banner -->
 <!-- team -->
-	<div class="team">
-		<div class="container">
-			<h3>Meet Our Amazing Team</h3>
-			<div class="agileits_team_grids">
-				<div class="col-md-3 agileits_team_grid">
-					<img src="images/32.jpg" alt=" " class="img-responsive" />
-					<h4>Martin Paul</h4>
-					<p>Manager</p>
-					<ul class="agileits_social_icons agileits_social_icons_team">
-						<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 agileits_team_grid">
-					<img src="images/33.jpg" alt=" " class="img-responsive" />
-					<h4>Michael Rick</h4>
-					<p>Supervisor</p>
-					<ul class="agileits_social_icons agileits_social_icons_team">
-						<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 agileits_team_grid">
-					<img src="images/34.jpg" alt=" " class="img-responsive" />
-					<h4>Thomas Carl</h4>
-					<p>Supervisor</p>
-					<ul class="agileits_social_icons agileits_social_icons_team">
-						<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 agileits_team_grid">
-					<img src="images/35.jpg" alt=" " class="img-responsive" />
-					<h4>Laura Lee</h4>
-					<p>CEO</p>
-					<ul class="agileits_social_icons agileits_social_icons_team">
-						<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-						<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
 <!-- //team -->
 <!-- testimonials -->
-	<div class="testimonials">
-		<div class="container">
-			<h3>Testimonials</h3>
-				<div class="w3_testimonials_grids">
-					<div class="wmuSlider example1 animated wow slideInUp" data-wow-delay=".5s">
-						<div class="wmuSliderWrapper">
-							<article style="position: absolute; width: 100%; opacity: 0;"> 
-								<div class="banner-wrap">
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>Andrew Smith <span>Customer</span></h4>
-									</div>
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>Thomson Richard <span>Customer</span></h4>
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</article>
-							<article style="position: absolute; width: 100%; opacity: 0;"> 
-								<div class="banner-wrap">
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>Crisp Kale <span>Customer</span></h4>
-									</div>
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>John Paul <span>Customer</span></h4>
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</article>
-							<article style="position: absolute; width: 100%; opacity: 0;"> 
-								<div class="banner-wrap">
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>Rosy Carl <span>Customer</span></h4>
-									</div>
-									<div class="col-md-6 w3_testimonials_grid">
-										<p><i class="fa fa-quote-right" aria-hidden="true"></i>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis 
-											voluptatibus maiores alias consequatur aut perferendis doloribus asperiores 
-											repellat.</p>
-										<h4>Rockson Doe <span>Customer</span></h4>
-									</div>
-									<div class="clearfix"> </div>
-								</div>
-							</article>
-						</div>
-					</div>
-					<script src="js/jquery.wmuSlider.js"></script> 
-					<script>
-						$('.example1').wmuSlider();         
-					</script> 
-				</div>
-		</div>
-	</div>
 <!-- //testimonials -->
 <!-- newsletter -->
 	<div class="newsletter">
@@ -428,14 +300,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script src="js/bootstrap.min.js"></script>
 <script>
 $(document).ready(function(){
-    $(".dropdown").hover(            
+    $(".dropdown").hover(
         function() {
             $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
+            $(this).toggleClass('open');
         },
         function() {
             $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
+            $(this).toggleClass('open');
         }
     );
 });
@@ -448,36 +320,30 @@ $(document).ready(function(){
 				containerID: 'toTop', // fading element id
 				containerHoverID: 'toTopHover', // fading element hover id
 				scrollSpeed: 1200,
-				easingType: 'linear' 
+				easingType: 'linear'
 				};
 			*/
-								
 			$().UItoTop({ easingType: 'easeOutQuart' });
-								
 			});
 	</script>
 <!-- //here ends scrolling icon -->
 <script src="js/minicart.js"></script>
 <script>
 		paypal.minicart.render();
-
 		paypal.minicart.cart.on('checkout', function (evt) {
 			var items = this.items(),
 				len = items.length,
 				total = 0,
 				i;
-
 			// Count the number of each item in the cart
 			for (i = 0; i < len; i++) {
 				total += items[i].get('quantity');
 			}
-
 			if (total < 3) {
 				alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
 				evt.preventDefault();
 			}
 		});
-
 	</script>
 </body>
 </html>
